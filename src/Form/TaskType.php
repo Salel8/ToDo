1ca3849176e40be7ec274a66f6463ab268675270
Sam <mehal.samir@hotmail.fr>
2024-02-02 11:55:05 +0100

    entities and controllers issue

---
+++ src/Form/TaskType.php
@@ -0,0 +1,31 @@
+<?php 
+// src/Form/TaskType.php
+namespace AppBundle\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content', TextareaType::class)
+            ->add('author')
+        ;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
